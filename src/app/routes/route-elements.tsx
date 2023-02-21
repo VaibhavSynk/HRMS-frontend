c3bd9494be07600c02d72be2e67a7b562b8e0967
Vaibhav <vaibhav.bombe@synkrama.com>
2023-02-21 18:10:46 +0530

    set added

--- src/app/routes/route-elements.tsx
+++ src/app/routes/route-elements.tsx
@@ -8,43 +8,45 @@ const Login = lazy(() => import("../container/auth/login"));
 const ForgotPassword = lazy(() => import("../container/auth/forgot-password"));
 const Profile = lazy(() => import("../container/profile/index"));
 const VerifyOtp = lazy(() => import("../container/auth/otpVerification/index"));
-const ResetPassword = lazy(() => import("../container/auth/resetPassword/index"));
+const ResetPassword = lazy(
+  () => import("../container/auth/resetPassword/index")
+);
 
 export const RouteElementsList: CommonRouteProps[] = [
-    {
-      path: RoutePath.dashboard,
-      element: <Dashboard />,
-      index: true,
-      isAuth: true
-    },
-    {
-      path: RoutePath.login,
-      element: <Login />,
-      index: false,
-      isAuth: false
-    },
-    {
-      path: RoutePath.forgotPassword,
-      element: <ForgotPassword />,
-      index: false,
-      isAuth: false
-    },
-    {
-      path: RoutePath.resetPassword,
-      element: <ResetPassword />,
-      index: false,
-      isAuth: true
-    },
-    {
-      path: RoutePath.profile,
-      element: <Profile />,
-      index: true,
-      isAuth: true
-    },
-    {
-      path: RoutePath.verifyOtp,
-      element: <VerifyOtp />,
-      index: false,
-      isAuth: false
-    },
-  ]
+  {
+    path: RoutePath.dashboard,
+    element: <Dashboard />,
+    index: true,
+    isAuth: true,
+  },
+  {
+    path: RoutePath.login,
+    element: <Login />,
+    index: false,
+    isAuth: false,
+  },
+  {
+    path: RoutePath.forgotPassword,
+    element: <ForgotPassword />,
+    index: false,
+    isAuth: false,
+  },
+  {
+    path: RoutePath.resetPassword,
+    element: <ResetPassword />,
+    index: false,
+    isAuth: true,
+  },
+  {
+    path: RoutePath.profile,
+    element: <Profile />,
+    index: true,
+    isAuth: true,
+  },
+  {
+    path: RoutePath.verifyOtp,
+    element: <VerifyOtp />,
+    index: false,
+    isAuth: false,
+  },
+];
